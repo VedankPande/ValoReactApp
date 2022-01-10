908341be3a952f2f4b1d98b3382aaf07f97d9fc5
Vedank Pande <vedank.pande@gmail.com>
2022-01-10 18:16:14 +0530

    initial commit

---
+++ src/components/AgentCard.js
@@ -0,0 +1,38 @@
+import React, { useEffect,useState } from 'react'
+import axios from 'axios'
+import "../styles.css"
+
+export function AgentCard(props){
+    const [agentData,setAgentData] = useState({});
+
+    useEffect(()=> {
+        console.log('running useEffect')
+        axios.get('https://valorant-api.com/v1/agents/').then(res=>{
+            var agentRes;
+
+            for (const agent of res.data['data']){
+                if (agent['displayName'] === props.agentName){ agentRes = agent}
+            }
+
+            setAgentData(agentRes);
+        })
+    },[])
+
+    return(
+        <div className='AgentCard'>
+            <h2>{agentData['displayName']}</h2> 
+            <img src={agentData['displayIcon']}></img>
+            <div className='AgentCardBody'>
+                <div className='AbilitiesBar'>
+                    <img src={agentData['abilities'][0]['displayIcon']} className='AbilitiesIcon'></img>
+                    <img src={agentData['abilities'][1]['displayIcon']} className='AbilitiesIcon'></img>
+                    <img src={agentData['abilities'][2]['displayIcon']} className='AbilitiesIcon'></img>
+                    <img src={agentData['abilities'][3]['displayIcon']} className='AbilitiesIcon'></img>
+                </div>
+            </div>
+        </div>
+    )
+
+
+    
+}
